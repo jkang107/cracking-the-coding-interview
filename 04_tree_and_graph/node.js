d2ece03c55753c6561b5204388621f773841f4b9
jinah <jkang107@gmail.com>
2017-01-08 21:15:02 +0900

    changed

---
+++ 04_tree_and_graph/node.js
@@ -0,0 +1,106 @@
+
+function Node(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    this.show = show;
+}
+
+function show() {
+    return this.data;
+}
+
+function BST() {
+    this.root = null;
+    this.insert = insert;
+    this.inOrder = inOrder;
+}
+
+function insert(data) {
+    var n = new Node(data, null, null);
+
+    if(this.root == null) {
+        this.root = n;
+    } else {
+        var current = this.root;
+        var parent;
+
+        while(true) {
+            parent = current;
+            if(data < current.data) {
+                current = current.left;
+                if(current == null) {
+                    parent.left = n;
+                    break;
+                }
+            } else {
+                current = current.right;
+                if(current == null) {
+                    parent.right = n;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function inOrder(node) {
+    if(!(node == null)) {
+        inOrder(node.left);
+        console.log(node.show() + " ");
+        inOrder(node.right);
+    }
+}
+
+function preOrder(node) {
+    if(!(node == null)) {
+        console.log(node.show() + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+}
+
+function postOrder(node) {
+    if(!(node == null)) {
+        postOrder(node.left);
+        postOrder(node.right);
+        console.log(node.show() + " ");
+    }
+}
+
+function getMin() {
+    var current = this.root;
+    while(!(current.left == null)) {
+        current = current.left;
+    }
+
+    return current.data;
+}
+
+function getMax() {
+    var current = this.root;
+    while(!(current.right == null)) {
+        current = current.right;
+    }
+
+    return current.data;
+
+}
+
+function find(data) {
+    var current = this.root;
+
+    while(current.data != data) {
+        if(data < current.data) {
+           current = current.left; 
+        } else {
+            current = current.right;
+        }
+
+        if(current == null) {
+            return null;
+        }
+    }
+    return current;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 cracking the code interview
+===========================
+craking the code 책의 연습문제를 매주 챕터별 2문제씩 푼다.
